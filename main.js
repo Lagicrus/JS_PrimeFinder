285069e54bb6d4c27785495c704423d1566cab0b
Lagicrus <samuelriches@gmail.com>
2016-11-28 15:31:39 +0000

    Primefinder

--- main.js
+++ main.js
@@ -1,26 +1,20 @@
 var primeCount = 0
+
 function primeChecker(number){
-  if (number % 2 == 0){}
-  else{
-    var prime = false
-    for (var numberInNumber = 2; numberInNumber < number; numberInNumber++){
-      if (number % numberInNumber == 0 && numberInNumber != number){
-        var prime = true
+  if (number % 2 != 0){
+      var prime = false;
+      for (var numberInNumber = 2; numberInNumber < number; numberInNumber++){
+        if (number % numberInNumber == 0 && numberInNumber != number){
+            prime = true;
+          }
         }
-      else{
-        if (prime != true){
-          var prime = false
-        }
+      if (prime == false){
+        primeCount += 1;
+        console.log(primeCount + " " + number + " is a prime");
       }
     }
-    if (prime == true){}
-    else{
-      primeCount += 1
-      console.log(primeCount + " " + number + " is a prime")
-    }
   }
-}
 
 for (var genNumber = 0; genNumber < 10000; genNumber++) {
-  primeChecker(genNumber)
+  primeChecker(genNumber);
 }
